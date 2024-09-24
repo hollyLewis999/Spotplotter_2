about to try fix the scaling issue

GUI.py
@@ -11,6 +11,7 @@ import copy
 from Processing import *
 from functools import partial
 import time
+import math 
 
 DARK = "#092934"
 LIGHT = "#FFFFFF"
@@ -295,6 +296,7 @@ def display_results(window):
     finish_button.place(relx=0.5, rely=0.9, anchor="center")
     
 def display_final_image(window, override =False):
+    #cv2.imshow("contours", resize_for_display(window.contour_img))
     add_to_history(window)
     # Clear the window
     for widget in window.winfo_children():
@@ -598,7 +600,7 @@ def create_editFrame(window, backToEdit = False):
         image=image_image_7,
         borderwidth=0,
         highlightthickness=0,
-        command=lambda: print("redo"),
+        command=lambda: redo(window),
         relief="flat",
         bg = DARK
     )
@@ -682,7 +684,7 @@ def create_editFrame(window, backToEdit = False):
     smallDots_label = Label(window, text="Size", font=(FONT, 12, 'bold'), fg=LIGHT, bg=DARK)
     smallDots_label.place(x=645, y=223)
     smallDots_slider = create_circular_slider(
-        window, min_val=0, max_val=10000,
+        window, min_val=1, max_val=100,
         position=(700, 190),
         command=lambda v: on_excludeSmallDots(window, v, False),
         initial_value=window.excludeSmallDots
@@ -829,7 +831,7 @@ def on_canvas_click(event, window, canvas):
     canvas.create_line(x-5, y+5, x+5, y-5, fill=DARK, width=2)
 
 def recalculate_grid(window):
-    if len(window.clicked_pointsx) < 16:
+    if len(window.clicked_pointsx) < 12:
         messagebox.showerror("Error", "Please select at least 16 points")
         return
    
@@ -860,11 +862,18 @@ def validate_and_proceed(window):
 
 
 def process_image(window):
-    # Apply your image processing steps here
+
     stretched, blurred, gray_image = stretch_and_gray(window.current_image, 90, 150)
     window.gray_image = gray_image
     binary_image, contour_img, final_binary, block_size = binarize(window.gray_image, window.current_image, excludeSmallDots=window.excludeSmallDots, contrast=window.contrast_value)
     
+    #############################FOR TESTING#######################
+
+
+    #################################################################
+
+
+
     window.contour_img = contour_img
     window.binarized_image = final_binary
     window.debug_image = np.stack((final_binary,) * 3, axis=-1)
@@ -946,7 +955,7 @@ def apply_crop(window):
         # Crop the image
         window.current_image = window.original_image[y_start:y_end, x_start:x_end]
     
-        
+        #cv2.imshow("Cropped", resize_for_display(window.current_image) )
         process_image(window)
     else:
         messagebox.showwarning("Warning", "Please select an area to crop.")
@@ -1126,7 +1135,7 @@ def on_contrast_change(window, value, backToEdit = False):
         backToEdit2 = False   
  
 def on_excludeSmallDots(window, value, backToEdit = False):
-    print("on_excludeSmallDots")
+    #print("on_excludeSmallDots")
     global backToEdit2
 
     if (backToEdit2 == False):
@@ -1135,7 +1144,7 @@ def on_excludeSmallDots(window, value, backToEdit = False):
         # Update the binarized image
         window.binarized_image = final_binary
         window.debug_image = np.stack((final_binary,) * 3, axis=-1)
-        print("am i resetting here?")
+        # print("am i resetting here?")
         # Update the history
         add_to_history(window)
         # Refresh the displayed images
@@ -1319,7 +1328,7 @@ def initialize_window_attributes(window):
     window.last_y = None
     window.update_undo_redo_buttons = update_undo_redo_buttons
     window.display_images = display_images
-    window.excludeSmallDots = 1000
+    window.excludeSmallDots = 15
     window.contrast_value = 20
     window.image_paths = []
     window.current_image_index = 0

Processing.py
@@ -12,6 +12,15 @@ from scipy.stats import linregress
 import matplotlib.pyplot as plt
 import math
 from scipy import ndimage
+######################################For scaling
+COLOUMS = 12
+
+#max radius = width/(dots across*2) 
+
+
+
+
+
 
 # d8888b. d888888b .d8888. d8888b. db       .d8b.  db    db 
 # 88  `8D   `88'   88'  YP 88  `8D 88      d8' `8b `8b  d8' 
@@ -114,16 +123,18 @@ def combine_masks(circles_mask, gridlines_mask, yellow_areas_mask, counts_mask, 
 # Y8b  d8   .88.   88 `88. Y8b  d8 88booo. 88.     db   8D 
 #  `Y88P' Y888888P 88   YD  `Y88P' Y88888P Y88888P `8888Y' 
                                                          
-
+#max area of the blocks = pi*maxr^2
 def findBlobs(binary_image, min_area, max_area, thickness=2):
+    #what is the max area?
+
     # Find contours in the binary image
     contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Create a color image to draw on
     result_image = cv2.cvtColor(binary_image, cv2.COLOR_GRAY2BGR)
-    cv2.imshow('contours', resize_for_display(result_image))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('contours', resize_for_display(result_image))
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     x_coords =[]
     y_coords=[]
     for contour in contours:
@@ -156,18 +167,26 @@ def findBlobs(binary_image, min_area, max_area, thickness=2):
 
 def detect_and_draw_circles(binary_image, gray_image, noClusters, min_radius=50, max_radius=140, param1=50, param2=28):
     """Detect circles in the image and draw grid, yellow areas, and counts."""
+    height, width = binary_image.shape
+    max_radius = int(width/24)
+    min_radius = int(max_radius/3)
+    max_area = max_radius**2*(math.pi)
+    min_area = min_radius**2*(math.pi)
+    # print("radiuses")
+    # print(max_radius)
+    # print(min_radius)
+    minDist = min_radius*2
     circles = cv2.HoughCircles(
         gray_image,
         cv2.HOUGH_GRADIENT,
         dp=0.9,
-        minDist=200,
+        minDist=minDist,
         param1=param1,
         param2=param2,
         minRadius=min_radius,
         maxRadius=max_radius
     )
     counts = np.zeros((8, 12))
-    height, width = gray_image.shape
 
     if circles is None:
         print("No circles detected")
@@ -175,7 +194,7 @@ def detect_and_draw_circles(binary_image, gray_image, noClusters, min_radius=50,
 
     if noClusters:
         print("Using blob detection")
-        x_coords, y_coords, marked_image = findBlobs(binary_image, 1500, 20000)
+        x_coords, y_coords, marked_image = findBlobs(binary_image, min_area, max_area)
     elif circles is not None:
         circles = np.round(circles[0, :]).astype(int)
         x_coords = circles[:, 0]
@@ -190,7 +209,7 @@ def detect_and_draw_circles(binary_image, gray_image, noClusters, min_radius=50,
         except ValueError as e:
             print(f"Error in grid calculation: {e}")
             print("Attempting blob detection with looser parameters")
-            x_coords, y_coords, marked_image = findBlobs(binary_image, 1000, 25000)  # Looser parameters
+            x_coords, y_coords, marked_image = findBlobs(binary_image,min_area/1.5, max_area*1.5)  # Looser parameters
             if len(x_coords) < 2 or len(y_coords) < 2:
                 raise ValueError("Unable to detect sufficient blobs for grid calculation")
 
@@ -209,7 +228,15 @@ def detect_and_draw_circles(binary_image, gray_image, noClusters, min_radius=50,
         for (x, y, r) in circles:
             cv2.circle(marked_image, (x, y), r, (0, 0, 255), 2)
             cv2.circle(marked_image, (x, y), 2, (0, 0, 255), 3)
+    
+    # cv2.imshow("marked_image", resize_for_display(marked_image)) 
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
+    # x_coords, y_coords, marked_image = findBlobs(binary_image, min_area, max_area)
+    # cv2.imshow ("marked_image", resize_for_display(marked_image)) 
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return counts, marked_image
 
 
@@ -227,7 +254,7 @@ def calculate_grid(x_coords, y_coords, width, height, binarized_image, gray_imag
         sorted_coords = np.sort(coords)
         diffs = np.diff(sorted_coords)
         median_diff = np.median(diffs)
-        threshold = median_diff * 2  # Adjust this factor if needed
+        threshold = max(median_diff *2,10) # Adjust this factor if needed
         
         clusters = []
         current_cluster = [sorted_coords[0]]
@@ -255,7 +282,7 @@ def calculate_grid(x_coords, y_coords, width, height, binarized_image, gray_imag
             plt.show()
         
         return cluster_means
-
+    #print(x_coords)
     x_clusters = find_clusters(x_coords)
     y_clusters = find_clusters(y_coords)
     
@@ -264,8 +291,13 @@ def calculate_grid(x_coords, y_coords, width, height, binarized_image, gray_imag
     
     x_diffs = np.diff(x_clusters)
     y_diffs = np.diff(y_clusters)
-    lowerBound = 200
-    upperBound = 320
+    # print("DIFFERECES")
+    # print(x_diffs)
+    # lowerBound = 200
+    # upperBound = 320
+    #######CHANGES HERE
+    lowerBound = width/30
+    upperBound = width/10
     filtered_x_diffs = [x for x in x_diffs if lowerBound <= x <= upperBound]
     filtered_y_diffs = [y for y in y_diffs if lowerBound <= y <= upperBound]
     if not filtered_x_diffs and not filtered_y_diffs:
@@ -388,7 +420,12 @@ def quantify_grid(binary_image, marked_image, grid_start_x, grid_start_y, cell_s
             else:
                 # Color it dark grey and don't count it
                 colored_image[component] = [64, 64, 64]  # Dark grey
-    
+    height, width = binary_image.shape
+    print("RESULTS")
+    print("counts" + str(counts[0][0]))
+    print("width" + str(width))
+    counts = (np.round((counts / (width**2)) * 1000000)).astype(int)
+
     # Combine the colored_image with the marked_image
     marked_image = cv2.addWeighted(marked_image, 1, colored_image, 0.5, 0)
     
@@ -406,8 +443,8 @@ def quantify_grid(binary_image, marked_image, grid_start_x, grid_start_y, cell_s
             text_y = int(y1 + cell_size / 2)
             cv2.putText(marked_image, str(counts[row, col]), (text_x - 20, text_y + 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 4)
-    
-    print(split_and_process(counts))
+    ######UNCOMMENT HERE 
+    #print(split_and_process(counts))
     return counts, marked_image
 
 
@@ -439,26 +476,24 @@ def stretch_and_gray(original_image, lower_bound, upper_bound, show_images=False
 
 def binarize(gray_image, original_image, contrast = 20,excludeSmallDots = 1000, show_images=False):
     """Binarize the grayscale image and perform contour detection."""
-   
+    
     block_size, divisor_c = 151, 15
     c = max(-50, min(int(-contrast), -1))
     binary_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
-                                       cv2.THRESH_BINARY, block_size, c)
-                                       
+                                       cv2.THRESH_BINARY, block_size, c)    
+
     contour_img = original_image.copy()
     final_binary = np.zeros_like(binary_image)
     contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    
-
-
-
-
-
+    height, width = binary_image.shape
+    excludeSmallDots = int((width*(excludeSmallDots/5000))**2)
+    #print(width)
+    #print(excludeSmallDots)
 
     areas = [cv2.contourArea(cntr) for cntr in contours]
     median_area = np.median(areas) if areas else 0
-    
+    excludeSmallDots
     for cntr in contours:
         area = cv2.contourArea(cntr)
         if area > excludeSmallDots: